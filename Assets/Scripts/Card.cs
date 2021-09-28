87c44d42fa663495242d4dc0a18b7ee200694f77
atlass-dev <73686943+atlass-dev@users.noreply.github.com>
2021-09-28 15:06:06 +0500

    Part 1

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Card/New card", fileName = "New card", order = 51)]
+public class Card : ScriptableObject
+{
+    public Sprite Icon;
+    public GameObject Prefab;
+    public int Cost;
+}
---
+++ Assets/Scripts/CardHolder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CardHolder : MonoBehaviour
+{
+    [SerializeField] private Transform _cardHolderPosition;
+    [SerializeField] private GameObject _card;
+    [SerializeField] private Card[] _cardSO;
+    private int _cardsAmmount;
+    private GameObject[] _plantedCards;
+
+    private int _cost;
+    private Sprite _icon;
+
+    private void Start()
+    {
+        _cardsAmmount = _cardSO.Length;
+        _plantedCards = new GameObject[_cardsAmmount];
+
+        for (int i = 0; i < _cardsAmmount; i++)
+            CreateCard(i);
+    }
+
+    private void CreateCard(int index)
+    {
+        var card = Instantiate(_card, _cardHolderPosition);
+
+        _plantedCards[index] = card;
+
+        _icon = _cardSO[index].Icon;
+        _cost = _cardSO[index].Cost;
+
+        card.GetComponentInChildren<SpriteRenderer>().sprite = _icon;
+        card.GetComponentInChildren<TMP_Text>().text = _cost.ToString();
+    }
+}
